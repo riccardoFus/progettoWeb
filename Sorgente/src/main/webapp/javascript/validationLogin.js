ac0ec062d4a0747fc8d6deb4e57906be226a3d26
Cesare <cesare.lever@studenti.unitn.it>
2023-05-31 10:39:52 +0200

    Aggiunta script login dinamico

--- Sorgente/src/main/webapp/javascript/validationLogin.js
+++ Sorgente/src/main/webapp/javascript/validationLogin.js
@@ -1,28 +1,49 @@
 function isEmpty(x){
-    if(x=="")
+    if(x==="")
         return true;
-    else return false;
+    else
+        return false;
 }
 
 function checkInput(field, dataType){
-    if(isEmpty(field.value) && dataType=='username'){
-        setAlert('usernameMissing');
+    if(isEmpty(field.value)){
+        document.getElementById("labelError").innerHTML = "07: " + dataType + " mancante! Si prega di ricompilare";
+        return false;
+    }else{
+        document.getElementById("labelError").innerHTML = "";
+        return true;
     }
+}
 
-    else if(isEmpty(field.value) && dataType=='password'){
-        setAlert('passwordMissing');
-    }
+    function checkLogin(){
+        let form = document.getElementById("form");
+        return checkInput(form.elements.namedItem("username"), 'Username') && checkInput(form.elements.namedItem("password"), 'Password') && checkLoginValid(form);
 }
 
-function setAlert(trigger) {
-    switch (trigger) {
-        case 'usernameMissing':
-            return alert("Username mancante! Si prega di compilarlo");
-            break;
-        case 'passwordMissing':
-            return alert("Password mancante! Si prega di inserirla");
+    function checkLoginValid(form){
+    //Riceve l'errore
+        let esito = true;
+        let response;
+        let url = "logIn?username="+form.elements.namedItem("username").value+"&password="+form.elements.namedItem("password").value;
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("GET", url, false);
+        xhttp.onreadystatechange = function(){
+            if(this.readyState===4 && this.status===200){
+                response = this.response;
+                if(response === ""){
+                    document.getElementById("labelError").innerHTML="";
+                }
+                else{
+                    document.getElementById("labelError").innerHTML=response.split("\"")[1];
+                }
+            }
+        };
+        xhttp.send();
+        if(response === ""){
+            esito = true;
+        }else{
+            esito = false;
+        }
+        return esito;
+
     }
-}
-    function checkLogin(form){
-        return checkInput(form.elements['username'],'username') || checkInput(form.elements['password'], 'password');
-}
