736c4984946f0cee524a7672c6ad65876ca5acdb
Cesare <cesare.lever@studenti.unitn.it>
2023-06-09 17:22:23 +0200

    Aggiunti grafici

--- Sorgente/src/main/webapp/javascript/statistics.js
+++ Sorgente/src/main/webapp/javascript/statistics.js
@@ -15,20 +15,84 @@ function showTotalViewers() {
     xhttp.send();
 }
 
-function showIstogramma() {
-    let url = "StatisticsServlet?action=plot";
+function showIstogrammaDonazioni() {
+    let url = "StatisticsServlet?action=plotDonazioni";
     let xhttp = new XMLHttpRequest();
-    xhttp.open("POST", url, false);
+    xhttp.open("GET", url, true);
+    xhttp.responseType = "json";
     xhttp.onreadystatechange = function(){
         if(this.readyState===4 && this.status===200){
-            let response = this.response;
+            let jsonArray = this.response;
             pulisciMenu();
-            showWarning(response,"views");
-            document.getElementById("istogramma").style.color="salmom";
+            let mesi = [];
+            for(let i = 0; i < 12; i++){
+                mesi[i] = 0;
+            }
+            for (let i = 0; i < jsonArray.length; i++) {
+                let current_JSON_object  = JSON.parse(jsonArray[i]);
+                mesi[new Date(current_JSON_object["data"]).getMonth()] += current_JSON_object["quota"];
+            }
+
+            document.getElementById("divGrafico").style.visibility = "visible";
+
+            const chart = Highcharts.chart('divGrafico', {
+                chart: {
+                    type: 'bar'
+                },
+                title: {
+                    text: 'Grafico Donazioni'
+                },
+                xAxis: {
+                    categories:['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre']
+                },
+                series: [{
+                    name: 'Somma in euro',
+                    data: [mesi[0], mesi[1], mesi[2], mesi[3], mesi[4], mesi[5], mesi[6], mesi[7], mesi[8], mesi[9], mesi[10], mesi[11]]
+                }]
+            });
         }
     }
     xhttp.send();
 }
+
+function showIstogrammaVisite() {
+    let url = "StatisticsServlet?action=plot";
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, true);
+    xhttp.responseType = "json";
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let jsonArray = this.response;
+            pulisciMenu();
+            let pagine = [];
+            let visite = [];
+            for (let i = 0; i < jsonArray.length; i++) {
+                let current_JSON_object  = JSON.parse(jsonArray[i]);
+                pagine[i] = current_JSON_object["page"];
+                visite[i] = current_JSON_object["visits"];
+            }
+
+            document.getElementById("divGrafico").style.visibility = "visible";
+                const chart = Highcharts.chart('divGrafico', {
+                    chart: {
+                        type: 'bar'
+                    },
+                    title: {
+                        text: 'Grafico visite'
+                    },
+                    xAxis: {
+                        categories: pagine
+                    },
+                    series: [{
+                        name: 'Visite',
+                        data: visite
+                    }]
+                });
+            }
+        }
+        xhttp.send();
+}
+
 function showWarning(msg, id){
     //imposta il messaggio dello warning e mostralo
     let el = document.getElementById(id);
@@ -109,7 +173,6 @@ function showAderenteSubscriptions(){
         if(xhttp.readyState===4 && xhttp.status===200){
             let jsonArray = this.response;
             let table = document.getElementById("showUsers");
-
             pulisciMenu();
 
             if(jsonArray === null){
@@ -182,4 +245,6 @@ function pulisciMenu(){
     label1.style.visibility = "hidden";
     let label2 = document.getElementById("text1");
     label2.style.visibility = "hidden";
+    document.getElementById("divGrafico").style.visibility = "hidden";
 }
+
